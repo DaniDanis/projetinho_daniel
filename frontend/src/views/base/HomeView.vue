6be90c56b4ba579d274ed56955220cf772aad581
Daniel Dantas da Silva <daniel.dantas.98@gmail.com>
2023-02-16 15:08:16 -0300

    Adicionado formulario basico para gerar destinos

--- frontend/src/views/base/HomeView.vue
+++ frontend/src/views/base/HomeView.vue
@@ -1,72 +1,56 @@
 <template>
-  <v-container class="fill-height">
+  <v-container class="fill-height home-page">
     <v-responsive class="d-flex align-center text-center fill-height">
-      <v-img contain height="240" src="@/assets/logo.svg" />
-      <blockquote class="blockquote text-h5">
-        &#8220;Primeiro, resolva o problema. Depois, escreva o código.&#8221;
-        <footer>
-          <small>
-            <em>&mdash; John Johnson &mdash;</em>
-          </small>
-        </footer>
-      </blockquote>
+      <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+      >
+        <v-text-field
+          v-model="nome"
+          :counter="100"
+          label="Nome"
+          required
+        ></v-text-field>
+        <v-select
+          v-model="tiposTurismo"
+          :items="tiposDeTurismo"
+          label="Seu tipo de turismo preferido <3"
+          multiple
+          chips
+          required
+        ></v-select>
+        <v-select
+          v-model="destinos"
+          :items="tiposDestino"
+          label="Tipo de viagem"
+          multiple
+          chips
+          required
+        ></v-select>
+        <v-select
+          v-model="climas"
+          :items="tiposDeClima"
+          label="Clima de preferência"
+          multiple
+          chips
+          required
+        ></v-select>
 
-      <div class="text-body-1 mb-n1 pt-4">Bem-vindo ao template</div>
-      <h1 class="text-h2 font-weight-bold">Djàvue</h1>
-      <code>Vue3 + Vuetify + Vite</code>
+        <v-checkbox
+          v-model="checkbox"
+          label="Ainda não sei se vou usar o checkbox"
+          required
+        ></v-checkbox>
 
-      <div class="py-6" />
-
-      <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            color="primary"
-            :to="{ name: 'base-getstarted' }"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            variant="flat"
-            class="my-4">
-            <v-icon icon="mdi-speedometer" size="large" start />
-            Get Started
-          </v-btn>
-          <v-btn
-            v-if="!loggedUser"
-            color="primary"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            variant="flat"
-            :to="{ name: 'accounts-login' }"
-            class="my-4">
-            <v-icon icon="mdi-account-arrow-right-outline" size="large" start />
-            Login
-          </v-btn>
-          <v-btn
-            v-else
-            color="primary"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            variant="flat"
-            :to="{ name: 'accounts-logout' }">
-            <v-icon icon="mdi-account-arrow-right-outline" size="large" start />
-            Logout
-          </v-btn>
-          <v-btn
-            v-if="loggedUser"
-            color="primary"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            variant="flat"
-            :to="{ name: 'tasks-list' }"
-            class="my-4">
-            <v-icon icon="mdi-folder-star-multiple" size="large" start />
-            tarefas
-          </v-btn>
-        </v-col>
-      </v-row>
+        <v-btn
+          color="success"
+          class="me-4"
+          @click="PesquisaDestino"
+        >
+          Pesquisar Destinos
+        </v-btn>
+      </v-form>
     </v-responsive>
   </v-container>
 </template>
@@ -76,8 +60,35 @@ import { mapState } from "pinia"
 import { useAccountsStore } from "@/stores/accountsStore"
 
 export default {
+  data: () => ({
+      valid: true,
+      nome: '',
+      climas: null,
+      destinos: null,
+      tiposTurismo: null,
+      tiposDeClima: [
+        'Quente',
+        'Frio',
+      ],
+      tiposDestino: [
+        'Nacional',
+        'Internacional'
+      ],
+      tiposDeTurismo: [
+        'Aventura',
+        'Lazer',
+        'Négocios',
+        'Eventos'
+      ],
+      checkbox: false,
+    }),
   computed: {
     ...mapState(useAccountsStore, ["loggedUser"]),
   },
+  methods: {
+      async PesquisaDestino () {
+        console.log("Tem nada aqui ainda meu amiguinho")
+      }
+    },
 }
 </script>
