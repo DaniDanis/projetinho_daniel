69bc9423ba2f300ff52ce3a439f94009d3cae78b
Daniel Dantas da Silva <daniel.dantas.98@gmail.com>
2023-02-24 15:34:00 -0300

    Pequena refatorada no codigo, e cards vou deixar assim por enquanto

--- frontend/src/components/ExibeDestinos.vue
+++ frontend/src/components/ExibeDestinos.vue
@@ -5,51 +5,19 @@
               class="mx-auto"
               max-width="1000"
             >
-            <v-card-title>Uma opção de viagem perfeita para você é {{destinoPerfeito.destino}}</v-card-title>
-              <v-img
-                class="align-end text-white"
-                height="400"
-                :alt="'Imagem do destino ' + destinoPerfeito.destino"
-                :src="destinoPerfeito.img"
-                cover
-              >
-              </v-img>
-          
-              <v-card-subtitle class="pt-4">
-                {{destinoPerfeito.destino}}
-              </v-card-subtitle>
-          
-              <v-card-text>
-                <div>O clima desse destino é {{ destinoPerfeito.clima }}</div>
-          
-                <div>E é uma viagem {{ destinoPerfeito.tipoDestino }}</div>
-              </v-card-text>          
-              <v-card-actions>
-      <v-btn
-        color="orange-lighten-2"
-        variant="text"
-        @click="show = !show"
-      >
-        Saiba mais
-      </v-btn>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
-        @click="show = !show"
-      ></v-btn>
-    </v-card-actions>
-
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
-
-        <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-        </v-card-text>
-      </div>
-    </v-expand-transition>
+                <v-card-title>Uma opção de viagem perfeita para você é {{destinoPerfeito.destino}}</v-card-title>
+                <v-img
+                    class="align-end text-white"
+                    height="400"
+                    :alt="'Imagem do destino ' + destinoPerfeito.destino"
+                    :src="destinoPerfeito.img"
+                    cover
+                ></v-img>        
+                <v-card-subtitle class="pt-4">{{destinoPerfeito.destino}}</v-card-subtitle>          
+                <v-card-text>
+                    <div>O clima desse destino é {{ destinoPerfeito.clima }}</div>          
+                    <div>E é uma viagem {{ destinoPerfeito.tipoDestino }}</div>
+                </v-card-text>          
             </v-card>
         </div>
         <div v-for="destinoAlternativo in destinosAlternativos" :key="destinoAlternativo.id">
@@ -57,34 +25,19 @@
               class="mx-auto"
               max-width="1000"
             >
-            <v-card-title>Uma opção de viagem alternativa para você é {{destinoAlternativo.destino}}</v-card-title>
-              <v-img
+                <v-card-title>Uma opção de viagem alternativa para você é {{destinoAlternativo.destino}}</v-card-title>
+                <v-img
                 class="align-end text-white"
                 height="400"
                 :alt="'Imagem do destino ' + destinoAlternativo.destino"
                 :src="destinoAlternativo.img"
                 cover
-              >
-              </v-img>
-          
-              <v-card-subtitle class="pt-4">
-                {{destinoAlternativo.destino}}
-              </v-card-subtitle>
-          
-              <v-card-text>
-                <div>O clima desse destino é {{ destinoAlternativo.clima }}</div>
-          
-                <div>E é uma viagem {{ destinoAlternativo.tipoDestino }}</div>
-              </v-card-text>          
-              <!-- <v-card-actions>
-                <v-btn color="orange">
-                  Share
-                </v-btn>
-          
-                <v-btn color="orange">
-                  Explore
-                </v-btn>
-              </v-card-actions> -->
+                ></v-img>
+                <v-card-subtitle class="pt-4">{{ destinoAlternativo.destino }}</v-card-subtitle>
+                <v-card-text>
+                    <div>O clima desse destino é {{ destinoAlternativo.clima }}</div>            
+                    <div>E é uma viagem {{ destinoAlternativo.tipoDestino }}</div>
+                </v-card-text>          
             </v-card>
         </div>
     </div>
